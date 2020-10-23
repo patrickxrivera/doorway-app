325a4a0b5ee5043f8692a73b5e836a2c6a499fc0
pxr13 <patrick.x.rivera@gmail.com>
2020-10-23 16:08:33 -0500

    Add models and migrations

---
+++ backend/src/migrations/20201023205636-create-referral-codes.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const tableName = "referralCodes";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable(tableName, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        references: {
+          model: "users", 
+          key: "id" 
+        },
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      code: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING
+      },
+      revokedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable(tableName);
+  }
+};
